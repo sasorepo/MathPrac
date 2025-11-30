e6f4e647cb391255df1c3590a4325ba1dabbd733
shashwath <shashwath.manjunath@gmail.com>
2025-11-30 11:14:49 -0800

    Initial Commit

---
+++ MathPrac/MathPracApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathPracApp.swift
+//  MathPrac
+//
+//  Created by Shashwath Manjunath on 11/30/25.
+//
+
+import SwiftUI
+
+@main
+struct MathPracApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MathPracTests/MathPracTests.swift
@@ -0,0 +1,17 @@
+//
+//  MathPracTests.swift
+//  MathPracTests
+//
+//  Created by Shashwath Manjunath on 11/30/25.
+//
+
+import Testing
+@testable import MathPrac
+
+struct MathPracTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
